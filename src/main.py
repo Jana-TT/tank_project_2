ac2762b8f650e72df159de0b581e908defb14f63
Jana-TT <janatt2056@gmail.com>
2024-05-07 14:13:51 -0500

    me dumb

--- src/main.py
+++ src/main.py
@@ -91,7 +91,7 @@ def read_root():
     return {"Hello": "Worldppp, weeeepooooo, why port fail?"}
 
 def main():
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
     main()
