32b22d98367d4e2fe77e919ae36db6b003be8e68
Jana-TT <janatt2056@gmail.com>
2024-05-30 21:08:29 -0500

    wrong type

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ class TankDataTransform(BaseModel):
     InchesToESD: Optional[float]
     TimeUntilESD: Optional[float]
     Capacity: Optional[float]
-    PercentFull: Optional[float]
+    PercentFull: Optional[int]
 
 
 class TankType(Enum):
