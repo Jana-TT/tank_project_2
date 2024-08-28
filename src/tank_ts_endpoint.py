7a40e34dd2b044383fce67cc73df2c481e230467
Jana-TT <janatt2056@gmail.com>
2024-08-28 14:52:31 -0500

    modified the tank_ts_endpoint

--- src/tank_ts_endpoint.py
+++ src/tank_ts_endpoint.py
@@ -9,13 +9,14 @@ from src.pool import PG
 
 class TankTsDataTransform(BaseModel):
     tank_metric: str
+    tank_type: str
     uom: str
     timestamps: List[datetime]
     values: List[float]
 
 
 class GetTanksTimeDataReq(BaseModel):
-    source_key: str = Field(default="6941901")
+    source_key: set[str] = Field(default=["9884001"])
 
 
 class TankTsDataTransformResponse(BaseModel):
@@ -23,10 +24,10 @@ class TankTsDataTransformResponse(BaseModel):
 
 
 TANK_TS_QUERY = """--sql
-    SELECT td.ts, td.value, dc.source_key AS source_key, dc.metric_nice_name AS tank_name, dc.uom
+    SELECT td.ts, td.value, dc.source_key AS source_key, dc.metric_nice_name, dc.uom
     FROM sdm_dba.timeseries_data AS td
     JOIN sdm_dba.data_catalog AS dc ON td.key_metric = dc.key_metric
-    WHERE dc.source_key = :source_key
+    WHERE dc.source_key = ANY(:source_key::VARCHAR[])
 """
 
 
@@ -44,7 +45,7 @@ def transform_tank_ts_data(df: Optional[pl.DataFrame]) -> list[dict[str, Any]]:
 
     lf = lf.drop("key_metric")
 
-    tank_metrics = ["Level", "Volume", "InchesToESD"]
+    tank_metrics = ["Level", "Volume", "InchesUntilAlarm", "InchesToESD"]
     tank_types = ["Water", "Oil"]
 
     tank_metrics_str = "|".join(tank_metrics)
@@ -52,15 +53,26 @@ def transform_tank_ts_data(df: Optional[pl.DataFrame]) -> list[dict[str, Any]]:
 
     pattern = f"^(?<is_esd>ESD-)?(?<tank_type>{tank_types_str})Tank(?<tank_number>[0-9]*)(?<tank_metric>{tank_metrics_str})"
     lf = lf.with_columns(
-        separated_metrics=pl.col("tank_name").str.extract_groups(pattern)
+        separated_metrics=pl.col("metric_nice_name").str.extract_groups(pattern)
     )
     lf = lf.unnest("separated_metrics")
 
-    lf = lf.drop("tank_name", "is_esd", "tank_type", "tank_number")
+    lf = lf.drop("is_esd", "tank_number")
 
-    aggregated_lf = lf.group_by(["tank_metric", "uom"]).agg(
+    filtered_lf = lf.filter(~pl.col("metric_nice_name").str.ends_with("ID"))
+
+    ts_to_sec = filtered_lf.with_columns(
+        pl.col("ts")
+        .dt.strftime("%Y-%m-%d %H:%M:%S")
+        .str.strptime(pl.Datetime)
+        .alias("ts")
+    )
+
+    aggregated_lf = ts_to_sec.group_by(["tank_metric", "tank_type", "uom"]).agg(
         [pl.col("ts").alias("timestamps"), pl.col("value").alias("values")]
     )
+
+    aggregated_lf = aggregated_lf.sort("tank_metric")
 
     result = aggregated_lf.collect()
     return result.to_dicts()
