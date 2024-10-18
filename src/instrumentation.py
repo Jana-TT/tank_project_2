b7920f9897387bfc28d1feef379e714d3c4664d0
Jana-TT <janatt2056@gmail.com>
2024-10-18 17:35:48 -0500

    resource name

--- src/instrumentation.py
+++ src/instrumentation.py
@@ -1,13 +1,18 @@
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.asyncpg import AsyncPGInstrumentor
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 
 def setup_tracing():
+    # resource name
+    resource = Resource.create({"service.name": "tanks_api"})
+
     # Setting  up the tracer Provider and Exporter
-    provider = TracerProvider()
+    provider = TracerProvider(resource=resource)
     exporter = OTLPSpanExporter()
 
     processor = BatchSpanProcessor(exporter)
@@ -20,3 +25,4 @@ def setup_tracing():
 def instrument_app(app):
     # Instrument the FastAPI app (instrument, the code to make it work)
     FastAPIInstrumentor.instrument_app(app)
+    AsyncPGInstrumentor().instrument()
